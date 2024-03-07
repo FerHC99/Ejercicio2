7f98f53c8da2ae26a8aced51e38a9c8514316729
FerHC99 <ferhc99@gmail.com>
2024-03-07 11:42:08 -0600

    Agregar archivos de proyecto.

---
+++ Model/DataAccess/DataAccess.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace Model.DataAccess
+{
+    public class DataAccess
+    {
+        string databaseConnection = ConfigurationManager.ConnectionStrings["DBEscuela"].ConnectionString;
+        //string databaseConnection = "Data Source=MARIFER\\VD_SERVER;Initial Catalog=EscuelaDB;user id=sa;password=Feb2024;";
+
+        public void SaveAlumno(Alumno alumno)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    SqlCommand command = new SqlCommand("[Escuela].[SaveAlumno]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter nombreParameter = new SqlParameter();
+                    nombreParameter.ParameterName = "Nombre";
+                    nombreParameter.Value = alumno.Nombre;
+
+                    SqlParameter apellidosParameter = new SqlParameter();
+                    apellidosParameter.ParameterName = "Apellidos";
+                    apellidosParameter.Value = alumno.Apellidos;
+
+                    SqlParameter promedioParameter = new SqlParameter();
+                    promedioParameter.ParameterName = "Promedio";
+                    promedioParameter.Value = alumno.Promedio;
+
+                    command.Parameters.Add(nombreParameter);
+                    command.Parameters.Add(apellidosParameter);
+                    command.Parameters.Add(promedioParameter);
+
+                    MessageBox.Show("Agregado exitosamente!");
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public List<Alumno> GetAlumnosList()
+        {
+            List<Alumno> resultado = new List<Alumno>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    SqlCommand command = new SqlCommand("[Escuela].[GetAlumnoList]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    connection.Open();
+                    var reader = command.ExecuteReader(); //Leer
+
+                    while (reader.Read())
+                    {
+                        Alumno alumno = new Alumno();
+                        alumno.Id = int.Parse(reader["Id"].ToString());
+                        alumno.Nombre = reader["Nombre"].ToString();
+                        alumno.Apellidos = reader["Apellidos"].ToString();
+                        alumno.Promedio = decimal.Parse(reader["Promedio"].ToString());
+                        resultado.Add(alumno);
+                    }
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return resultado;
+        }
+
+        public void Selection(object Fila, System.EventArgs s, DataGridView dataGridView)//Seleccionar una fila de DataGrid y ponerlo en TextBox
+        {
+            int Select;
+            Select = dataGridView.Rows.GetRowCount(DataGridViewElementStates.Selected);
+            if (Select > 0)
+            {
+                System.Text.StringBuilder fila = new System.Text.StringBuilder();
+
+                for (int i = 0; i < Select; i++)
+                {
+                    fila.Append(dataGridView.SelectedRows[i].Index.ToString());
+                    fila.Append(fila.ToString());
+                }
+            }
+        }
+
+        public void ModifyAlumno(Alumno alumnos)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    SqlCommand command = new SqlCommand("[Escuela].[ModifyAlumno]", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter idParameter = new SqlParameter();
+                    idParameter.ParameterName = "@Id";
+                    idParameter.Value = alumnos.Id;
+
+                    SqlParameter nombreParameter = new SqlParameter();
+                    nombreParameter.ParameterName = "@Nombre";
+                    nombreParameter.Value = alumnos.Nombre;
+
+                    SqlParameter apellidosParameter = new SqlParameter();
+                    apellidosParameter.ParameterName = "@Apellidos";
+                    apellidosParameter.Value = alumnos.Apellidos;
+
+                    SqlParameter promedioParameter = new SqlParameter();
+                    promedioParameter.ParameterName = "@Promedio";
+                    promedioParameter.Value = alumnos.Promedio;
+
+                    command.Parameters.Add(idParameter);
+                    command.Parameters.Add(nombreParameter);
+                    command.Parameters.Add(apellidosParameter);
+                    command.Parameters.Add(promedioParameter);
+
+                    MessageBox.Show("Modicado exitosamente!");
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    public void DeleteAlumno(Alumno alumno)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(databaseConnection))
+                {
+                    DialogResult Resultado = (DialogResult)MessageBox.Show("Estas seguro de eliminar este registro?", "Alerta!", MessageBoxButton.YesNo);
+                    if (Resultado == DialogResult.Yes)
+                    {
+                        SqlCommand command = new SqlCommand("[Escuela].[DeleteAlumno]", connection);
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlParameter idParameter = new SqlParameter();
+                        idParameter.ParameterName = "Id";
+                        idParameter.Value = alumno.Id;
+
+                        SqlParameter nombreParameter = new SqlParameter();
+                        nombreParameter.ParameterName = "Nombre";
+                        nombreParameter.Value = alumno.Nombre;
+
+                        SqlParameter apellidosParameter = new SqlParameter();
+                        apellidosParameter.ParameterName = "Apellidos";
+                        apellidosParameter.Value = alumno.Apellidos;
+
+                        SqlParameter promedioParameter = new SqlParameter();
+                        promedioParameter.ParameterName = "Promedio";
+                        promedioParameter.Value = alumno.Promedio;
+
+                        command.Parameters.Add(idParameter);
+                        command.Parameters.Add(nombreParameter);
+                        command.Parameters.Add(apellidosParameter);
+                        command.Parameters.Add(promedioParameter);
+
+                        MessageBox.Show("Se ha eliminado un registro");
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se ha eliminado un registro");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using Model;
+using Model.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class MainViewModel : NotificationObject
+    {
+        private DataAccess _dataAcces;
+        private string _nombre;
+        private string _apellidos;
+        private decimal _promedio;
+        private int _id;
+        private ICommand _agregarCommand;
+        private ICommand _modifyCommand;
+        private ICommand _clearCommand;
+        private ICommand _deleteCommand;
+        private List<Alumno> _alumnos;
+        private Alumno _alumnoSeleccionado;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if(value == _nombre) return;
+                _nombre = value;
+                RaisePropertyChanged(() => Nombre);
+            }
+        }
+        public string Apellidos
+        {
+            get { return _apellidos; }
+            set
+            {
+                if (value == _apellidos) return;
+                _apellidos = value;
+                RaisePropertyChanged(() => Apellidos);
+            }
+        }
+        public decimal Promedio
+        {
+            get { return _promedio; }
+            set
+            {
+                if (value == _promedio) return;
+                _promedio = value;
+                RaisePropertyChanged(() => Promedio);
+            }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                RaisePropertyChanged(() => Id);
+            }
+        }
+
+        public List<Alumno> Alumnos
+        {
+            get { return _alumnos; }
+            set
+            {
+                if (value == _alumnos) return;
+                _alumnos = value;
+                RaisePropertyChanged(() => Alumnos);
+            }
+        }
+
+        public Alumno AlumnoSeleccionado
+        {
+            get { return _alumnoSeleccionado; }
+            set
+            {
+                if (value == _alumnoSeleccionado) return;
+                _alumnoSeleccionado = value;
+
+                Nombre = _alumnoSeleccionado.Nombre;
+                Apellidos = _alumnoSeleccionado.Apellidos;
+                Promedio = _alumnoSeleccionado.Promedio;
+                Id = _alumnoSeleccionado.Id;
+
+                RaisePropertyChanged(() => AlumnoSeleccionado);
+            }
+        }
+        public ICommand AgregarCommand
+        {
+            get
+            {
+                if(_agregarCommand == null)
+                {
+                    _agregarCommand = new DelegateCommand(AgregarAlumno, () => true);
+                }
+                return _agregarCommand;
+            }
+        }
+        public ICommand ModifyCommand
+        {
+            get
+            {
+                if (_modifyCommand == null)
+                {
+                    _modifyCommand = new DelegateCommand(ModificarAlumno, () => true);
+                }
+                return _modifyCommand;
+            }
+        }
+        public ICommand DeleteCommand
+        {
+            get
+            {
+                if (_deleteCommand == null)
+                {
+                    _deleteCommand = new DelegateCommand(DeleteAlumno, () => true);
+                }
+                return _deleteCommand;
+            }
+        }
+
+        public ICommand ClearCommand
+        {
+            get
+            {
+                if (_clearCommand == null)
+                {
+                    _clearCommand = new DelegateCommand(ClearAlumno, () => true);
+                }
+                return _clearCommand;
+            }
+        }
+
+        public MainViewModel()
+        {
+            Alumnos = new List<Alumno>();
+            _dataAcces = new DataAccess();
+            Alumnos = _dataAcces.GetAlumnosList();
+        }
+
+        public void AgregarAlumno()
+        {
+            Alumno alumno = new Alumno();
+            alumno.Nombre = Nombre;
+            alumno.Apellidos = Apellidos;
+            alumno.Promedio = Promedio;
+            alumno.Id = 0;
+            _dataAcces.SaveAlumno(alumno);
+            Alumnos = _dataAcces.GetAlumnosList();
+            Nombre = String.Empty;
+            Apellidos = String.Empty;
+            Promedio = 0;
+            Id = 0;
+        }
+
+        public void ModificarAlumno()
+        {
+            Alumno alumno = new Alumno();
+            alumno.Nombre = Nombre;
+            alumno.Apellidos = Apellidos;
+            alumno.Promedio = Promedio;
+            alumno.Id = Id;
+            _dataAcces.ModifyAlumno(alumno);
+            Alumnos = _dataAcces.GetAlumnosList();
+            Nombre = String.Empty;
+            Apellidos = String.Empty;
+            Promedio = 0;
+        }
+
+        public void DeleteAlumno()
+        {
+            Alumno alumno = new Alumno();
+            alumno.Nombre = Nombre;
+            alumno.Apellidos = Apellidos;
+            alumno.Promedio = Promedio;
+            alumno.Id = Id;
+            _dataAcces.DeleteAlumno(alumno);
+            Alumnos = _dataAcces.GetAlumnosList();
+            Nombre = String.Empty;
+            Apellidos = String.Empty;
+            Promedio = 0;
+            Id = 0;
+
+        }
+        public void ClearAlumno()
+        {
+            Nombre = String.Empty;
+            Apellidos = String.Empty;
+            Promedio = 0;
+            Id = 0;
+        }
+    }
+}
